6db8420e34c409d3824f0c544537caaa650f2023
wasin.th <wasin.th@tailor-solutions.com>
2026-09-24 15:36:35 +0700

    Also change the display text

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_hit_count():
 @app.get("/")
 async def hello():
     count = get_hit_count()
-    return 'Hello 2222 I have been seen {} times.\n'.format(count)
+    return f"We have met for {count} times."
 
 @app.get("/current")
 async def current():
